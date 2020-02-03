83d87f75796ec5362c0f03b5d5711f808f6f508e
Steven <anguelr12@hotmail.com>
2020-02-03 14:09:11 -0500

    prueba

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
+import { log } from 'util';
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <app-ng-style></app-ng-style> 
+    <br>
+
+    <app-css></app-css>
+    <br>
+
+    <app-clases></app-clases>
+    <br><br>
+
+    <p [appResaltado]="'orange'">
+        Hola Mundo
+    </p>
+    
+    <br>
+    <app-ng-switch></app-ng-switch>
+  `,
+  styles: []
+})
+export class HomeComponent implements OnInit,OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  
+  constructor() {
+    console.log("constructor"); 
+  }
+
+  ngOnInit() {
+    console.log("ngOnInit");
+  }
+
+  ngOnChanges() {
+    console.log("ngOnChanges");
+  }
+  ngDoCheck() {
+    console.log("ngDoCheck");
+  }
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit");
+  }
+  ngAfterContentChecked() {
+    console.log("ngAfterContentChecked");
+  }
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit");
+  }
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked");
+  }
+  ngOnDestroy() {
+    console.log("ngOnDestroy");
+  }
+
+  
+
+}
